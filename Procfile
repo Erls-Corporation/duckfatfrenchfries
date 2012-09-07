2b6828ed07acbf2fc11d7a22ea1c9d39d798e125
Kelly Johnston <kjohnston@tendrilinc.com>
2012-09-06 22:51:02 -0600

    adds Procfile and file upload bits

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node persist/src/main.js
--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,9 @@
-
+(function(){
+	$('#upload').filestyle({
+				buttonText: 'Upload file',
+				classButton: 'btn-inverse',
+				icon: true,
+				classIcon: 'icon-arrow-up icon-white',
+				textField: false
+			});
+})(jQuery)
