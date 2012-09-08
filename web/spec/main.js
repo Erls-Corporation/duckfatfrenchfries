22bae7a73cae9cabc4aa47c572ede615c0c4ef6d
Kelly Johnston <kjohnston@tendrilinc.com>
2012-09-08 11:18:18 -0600

    adds jasmine and roughs out the specs

---
+++ web/spec/main.js
@@ -0,0 +1,45 @@
+describe("Defects", function(){
+
+
+	it("should set up a open a database", function(){
+
+	});
+
+	it("should create alert the user if websql is not supported", function(){
+
+	});
+
+	it("should create a document table", function(){
+
+	});
+
+	it("should create a defects table", function(){
+
+	});
+
+	it("should identify a good csv input", function(){
+
+	});
+
+	it("should identify a misformated csv input", function(){
+
+	});
+
+	it("should identify a potential SQL injection attack in the csv input", function(){
+
+	});
+
+	it("should add a new document row with a timestamp", function(){
+
+	});
+
+	it("should add a new day row", function(){
+
+	});
+
+	it("should hide the file input if the HTML5 File API is not supported", function(){
+
+	});
+
+	
+})
